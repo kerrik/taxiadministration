58937252f3bb940afbeaf54d181c0784aa6122c4
kerrik <itdrift@kerrik.se>
2016-12-15 15:59:31 +0100

    Första batchen frå Taxiadmin

--- taxiadmin/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ( $_SERVER['REQUEST_URI'] === '/'){    
-    include 'webroot/start.php';
-}else{    
-    $include = (stristr($_SERVER['REQUEST_URI'],'?', true)?stristr($_SERVER['REQUEST_URI'],'?', true): $_SERVER['REQUEST_URI']);
-    include "webroot/{$include}.php";
-}
